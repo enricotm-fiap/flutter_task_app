047edf1b602e685a4ec203ccf8273757869e84f6
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-08 20:48:42 -0300

    aula0810

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,8 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Supabase.initialize(
-    url: '',
-    anonKey: '',
+    url: 'https://ejkupkzjhajtyutxtvmv.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVqa3Vwa3pqaGFqdHl1dHh0dm12Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg0Mjg1NjAsImV4cCI6MjA0NDAwNDU2MH0.yQuhxHs0lk3iuYh2S4RcrKHh4meBI1Zi8MG5KiFpvaQ',
   );
 
   runApp(const MyApp());
---
+++ lib/provider/task_group_provider.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/models/task_group.dart';
+import 'package:todo_app/repository/task_group_repository.dart';
+
+class TaskGroupProvider extends ChangeNotifier {
+  final taskGroupRepository = TaskGroupRepository();
+
+  bool _isLoading = false;
+  bool get isLoading => _isLoading;
+  List<TaskGroup> _taskGroups = [];
+  List<TaskGroup> get taskGroups => _taskGroups;
+
+  Future<void> listTaskGroups() async {
+    _isLoading = true;
+    notifyListeners();
+    _taskGroups = await taskGroupRepository.listTaskGroups();
+    _isLoading = false;
+    notifyListeners();
+  }
+}
